6e0fdecd6126fcd26eea8c5e4c9fe49e45250182
SahilPanhotra <sahilpanhotra@gmail.com>
2022-11-23 23:32:14 +0530

    feat:recieve email from form to your email directly

---
+++ index.js
@@ -0,0 +1,27 @@
+//template_lcmqf1s
+//service_bzofxep
+//xrYcv5CqntpwvuqKL
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_bzofxep",
+      "template_lcmqf1s",
+      event.target,
+      "xrYcv5CqntpwvuqKL"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily down please contact me on twitter @sahilpanhotra"
+      );
+    });
+}
